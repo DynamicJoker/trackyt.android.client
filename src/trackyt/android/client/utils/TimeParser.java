9e7893704a1eede9b70f7ae0c148192f6a9e2ef7
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-01-19 17:23:34 +0200

    Time parser simplified

--- src/trackyt/android/client/utils/TimeParser.java
+++ src/trackyt/android/client/utils/TimeParser.java
@@ -3,19 +3,12 @@ package trackyt.android.client.utils;
 import trackyt.android.client.models.Time;
 public class TimeParser {
 	
-    private static final int ONE_MINUTE = 60;
-    private static final int ONE_HOUR = 3600;
-    
     public static Time convertToTime(int receivedTime){
     	Time time = new Time();
     	
-    	time.setHours(receivedTime / ONE_HOUR);
-    	if ((receivedTime %= ONE_HOUR) != 0){
-    		time.setMinutes(receivedTime / ONE_MINUTE);
-    		if ((receivedTime %= ONE_MINUTE) != 0) {
-    			time.setSeconds(receivedTime);
-    		}
-    	}
+    	time.setHours(receivedTime / 3600);
+    	time.setMinutes((receivedTime / 60) % 60);
+    	time.setSeconds(receivedTime % 60);
     	
     	return time;
     }
